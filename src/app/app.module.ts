56c125790a2e9a9e7929fb82555d3ea1f61c0878
Samuel Kim <SamuelKim@Owners-MacBook-Air.local>
2019-07-18 22:50:16 -0700

    removed buildpack

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 // good for adding routes to app
 import { RouterModule, Routes} from "@angular/router";
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomepageComponent } from './homepage/homepage.component';
