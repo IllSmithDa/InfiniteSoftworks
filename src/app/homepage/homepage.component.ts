e62757c8e449dc50a446f3faf27f501a85a0017d
Samuel Kim <SamuelKim@Owners-Air.attlocal.net>
2019-07-19 15:36:40 -0700

    added css options'

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -3,13 +3,43 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    // Checks if a element is in the view port. Returns true element is in viewport
+    let isInViewport = (elem) => {
+      let bounding = elem.getBoundingClientRect();
+      if (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    let header = document.getElementById("appTitle");
+    let secondHeader = document.getElementById("secondTitle");
+
+    // Event listener which will check if view port is in
+    window.addEventListener('scroll', function (event) {
+      if (isInViewport(secondHeader)) {
+        secondHeader.style.animationPlayState = "running";
+      }
+    }, false);
+
+    // Event listener which will check if view port is in
+    window.addEventListener('scroll', function (event) {
+      if (isInViewport(header)) {
+        header.style.animationPlayState = "runnung";
+      }
+    }, false);
   }
 
 }
